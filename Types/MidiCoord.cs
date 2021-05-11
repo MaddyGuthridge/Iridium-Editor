3169d8d340e89788e98bc8004b8f1a87a58d6ffe
Miguel Guthridge <hdsq@outlook.com.au>
2021-05-12 00:44:36 +1000

    Add MidiCoord class

---
+++ Types/MidiCoord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IridiumEditor.Types
+{
+    // Used to represent a coordinate: (x, y)
+    // where x is a MidiPos relative to the start of the parent object
+    // and y is the vertical position in terms of tracks
+    struct MidiCoord
+    {
+        // X coordinate
+        public MidiPos X { get; set; }
+
+        // Y coordinate (in terms of tracks)
+        public uint Y { get; set; }
+
+        public MidiCoord(MidiPos x, uint y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
