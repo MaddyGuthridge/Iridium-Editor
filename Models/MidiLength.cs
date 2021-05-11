cf3a2075e4e70d362e7dda9144d56368c7cc83a5
Miguel Guthridge <hdsq@outlook.com.au>
2021-05-12 00:05:54 +1000

    Create structs MidiLength and MidiPos

---
+++ Models/MidiLength.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iridium_Editor.Models
+{
+    // Used to represent the length of a MIDI object (eg note or phrase)
+    struct MidiLength
+    {
+        private int length;
+
+        public MidiLength(int ticks)
+        {
+            length = ticks;
+        }
+
+        public int ticks()
+        {
+            return length;
+        }
+    }
+}
---
+++ Models/MidiPos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iridium_Editor.Models
+{
+    // Used to represent the point at which a MIDI object begins or happens
+    struct MidiPos
+    {
+        public int Position { get; set; }
+
+        public MidiPos(int pos)
+        {
+            Position = pos;
+        }
+
+        // Return the position in terms of ticks
+        public int ticks()
+        {
+            return Position;
+        }
+    }
+}
