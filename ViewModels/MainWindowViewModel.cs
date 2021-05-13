5b701fa4cbe262f9cc269f64524b9cc5b5823d2a
Miguel Guthridge <hdsq@outlook.com.au>
2021-05-14 09:57:57 +1000

    Add status Bar and app icon

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -7,5 +7,8 @@ namespace IridiumEditor.ViewModels
     public class MainWindowViewModel : ViewModelBase
     {
         public string Title => "Iridium Editor";
+        public string Hint => "Hint Message";
+        public string NoteCount => "Note Count: 17048";
+        public string CursorPosition => "13.3.120 : 12";
     }
 }
