182517d3f8250d627369a29753f6efc8a47a49cf
Miguel Guthridge <hdsq@outlook.com.au>
2021-05-18 15:36:09 +1000

    Update MainWindow title when project is renamed
    Still kinda buggy though

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using ReactiveUI;
 using IridiumEditor.Views;
 
 namespace IridiumEditor.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ReactiveObject
     {
-        public string WindowTitle => App.Projects.GetProject(project).details.Name + " - Iridium";
+        private string windowTitle = "";
+        public string WindowTitle
+        {
+            get => windowTitle;
+            set => this.RaiseAndSetIfChanged(ref windowTitle, value);
+        }
         public string Hint => "Hint Message";
         public string NoteCount => "Note Count: 17048";
         public string CursorPosition => "13.3.120 : 12";
@@ -20,12 +25,19 @@ namespace IridiumEditor.ViewModels
         {
             project = App.Projects.CreateProject();
             window = win;
+            WindowTitle = GenWindowTitle();
+        }
+
+        private string GenWindowTitle()
+        {
+            return App.Projects.GetProject(project).details.Name + " - Iridium";
         }
 
         public void OnTitleMenuClick()
         {
             ProjectDetails details = new(project);
             details.ShowDialog(window);
+            WindowTitle = GenWindowTitle();
         }
     }
 }
